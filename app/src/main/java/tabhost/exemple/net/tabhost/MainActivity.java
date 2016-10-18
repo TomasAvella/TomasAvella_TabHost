506d2e0e1ab8efd7745246cc3ff5e34021b57ab8
Tomas Avella <tomasavella007@gmail.com>
2016-10-18 16:58:06 +0200

    Practica 2.11

--- app/src/main/java/tabhost/exemple/net/tabhost/MainActivity.java
+++ app/src/main/java/tabhost/exemple/net/tabhost/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends TabActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Resources res = getResources();
         TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
 
